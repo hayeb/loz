b9deacb960e1246dd492e96a8f3400ce1c856272
Haye Böhm <haye.bohm@axians.com>
2019-11-12 14:13:42 +0100

    First rudimentary version of parser

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use std::{fs, env};
 use std::rc::Rc;
 
-mod tokenizer;
+mod parser;
+
+#[macro_use]
+extern crate pest_derive;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,10 +13,6 @@ fn main() {
 
     let contents = fs::read_to_string(filename)
         .expect("Error reading from file: test/test.loz");
-    let res = tokenizer::tokenize(contents, Rc::new(String::from(filename)));
 
-    match res {
-        Ok(tokens) => println!("{:?}", tokens),
-        Err(err) => println!("{}", err)
-    }
+   println!("{:?}", parser::parse(&contents[..]));
 }
---
+++ src/parser.rs
@@ -0,0 +1,190 @@
+extern crate pest;
+
+use std::any::Any;
+
+use pest::error::Error;
+use pest::iterators::Pair;
+use pest::Parser;
+
+use Expression::StringLiteral;
+
+use crate::parser::Type::{Bool, NoType};
+use crate::parser::Expression::{CharacterLiteral, Number, Call, Variable};
+use std::hint::unreachable_unchecked;
+
+#[derive(Debug)]
+pub enum FunctionRule {
+    ConditionalRule(Expression, Expression),
+    ExpressionRule(Expression),
+}
+
+#[derive(Debug)]
+pub struct FunctionBody {
+    parameters: Vec<String>,
+    rules: Vec<FunctionRule>
+}
+
+#[derive(Debug)]
+pub enum Type {
+    NoType,
+    Bool,
+    Char,
+    String,
+    Int,
+}
+
+#[derive(Debug)]
+pub struct FunctionType {
+    from: Vec<Type>,
+    to: Type,
+}
+
+#[derive(Debug)]
+pub struct FunctionDeclaration {
+    name: String,
+    function_type: FunctionType,
+    function_bodies: Vec<FunctionBody>,
+}
+
+#[derive(Debug)]
+pub enum Expression {
+    StringLiteral(String),
+    CharacterLiteral(char),
+    Number(usize),
+    Identifier(String),
+    Call(String, Vec<String>),
+    Variable(String)
+}
+
+#[derive(Debug)]
+pub struct AST {
+    function_declarations: Vec<FunctionDeclaration>,
+    main: Expression,
+}
+
+#[derive(Parser)]
+#[grammar = "loz.pest"]
+pub struct LOZParser;
+
+pub fn parse(input: &str) -> Result<AST, Error<Rule>> {
+    let mut ast = LOZParser::parse(Rule::ast, input)?.next().unwrap();
+
+    println!("{:?}", ast);
+
+    Ok(to_ast(ast))
+}
+
+fn to_ast(pair: Pair<Rule>) -> AST {
+    match pair.as_rule() {
+        Rule::ast => {
+            let mut rules = pair.into_inner().peekable();
+            let mut function_declarations = Vec::new();
+
+            while let Some(rule) = rules.next() {
+                if rules.peek().is_some() {
+                    // Not the last one, function declaration.
+                    function_declarations.push(to_function_declaration(rule));
+                } else {
+                    // Not the last one, function declaration.
+                    return AST { function_declarations, main: to_expression(rule.into_inner().next().unwrap()) };
+                }
+            }
+            unreachable!()
+        }
+        _ => unreachable!()
+    }
+}
+
+fn to_function_declaration(pair: Pair<Rule>) -> FunctionDeclaration {
+    println!("to_function_declaration: {:?}", pair);
+    let mut inner_rules = pair.into_inner();
+    let name = inner_rules.next().unwrap().as_str();
+    let function_type = to_function_type(inner_rules.next().unwrap());
+    FunctionDeclaration {
+        name: name.to_string(),
+        function_type,
+        function_bodies: inner_rules.map(|b| to_function_body(b)).collect()
+    }
+}
+
+fn to_expression(pair: Pair<Rule>) -> Expression {
+    println!("to_expression: {:?}", pair);
+    let sub = pair.into_inner().next().unwrap();
+    match sub.as_rule() {
+        Rule::string_literal => StringLiteral(sub.into_inner().next().unwrap().as_str().to_string()),
+        Rule::char_literal => CharacterLiteral(sub.as_str().to_string().chars().nth(1).unwrap()),
+        Rule::number => Number(sub.as_str().parse::<usize>().unwrap()),
+        Rule::call => {
+            let mut subs = sub.into_inner();
+            let function = subs.next().unwrap().as_str();
+            let arguments = subs.map(|a| a.as_str().to_string()).collect();
+            Call(function.to_string(), arguments)
+        },
+        Rule::identifier => Variable(sub.as_str().to_string()),
+        _ => unreachable!()
+    }
+}
+
+fn to_function_type(pair: Pair<Rule>) -> FunctionType {
+    println!("to_function_type {:?}", pair);
+
+    let mut types = pair.into_inner().peekable();
+    let mut from_types = Vec::new();
+
+    while let Some(t) = types.next() {
+        if types.peek().is_some() {
+            // Not the last, from type
+            from_types.push(to_type(t));
+        } else {
+            // The last, to type
+            return FunctionType { from: from_types, to: to_type(t) };
+        }
+    }
+    unreachable!()
+}
+
+fn to_function_body(pair: Pair<Rule>) -> FunctionBody {
+    println!("to_function_body {:?}", pair);
+    let mut rules = pair.into_inner();
+    let parameters = to_parameter_names(rules.next().unwrap());
+    let function_rules = rules.map(to_function_rule).collect();
+    FunctionBody {parameters, rules: function_rules}
+}
+
+fn to_function_rule(pair: Pair<Rule>) -> FunctionRule {
+    println!("to_function_rule {:?}", pair);
+    match pair.as_rule() {
+        Rule::function_conditional_rule => {
+            let mut rules = pair.into_inner().next().unwrap().into_inner();
+            println!("rules: {:?}", rules);
+            let left = to_expression(rules.next().unwrap());
+            let right = to_expression(rules.next().unwrap());
+            FunctionRule::ConditionalRule(left, right)
+        },
+        Rule::function_expression_rule =>  {
+            let mut rules = pair.into_inner();
+            FunctionRule::ExpressionRule(to_expression(rules.next().unwrap()))
+        }
+        _ => unreachable!()
+    }
+}
+
+fn to_parameter_names(pair: Pair<Rule>) -> Vec<String> {
+    println!("to_function_header {:?}", pair);
+    pair.into_inner().map(|param| param.as_str().to_string()).collect()
+}
+
+fn to_type(pair: Pair<Rule>) -> Type {
+    match pair.as_rule() {
+        Rule::loz_type => {
+            match pair.as_span().as_str() {
+                "Bool" => Type::Bool,
+                "String" => Type::String,
+                "Int" => Type::Int,
+                "Char" => Type::Char,
+                _ => Type::NoType
+            }
+        }
+        _ => unreachable!()
+    }
+}
